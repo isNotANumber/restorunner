6ee030941fe5c013721d69b8e46c4ec31e66b65c
Kirill T <nan1911.work@gmail.com>
2024-11-19 18:33:32 +0500

    react-router-dom and routes addded

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,28 @@
-import MainPage from "../../pages/main/MainPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import MainPage from '../../pages/main/MainPage';
+import LoginPage from '../../pages/login/LoginPage';
+import FavoritesPage from '../../pages/favorites/FavoritesPage';
+import OfferPage from '../../pages/offer/OfferPage';
 
 function App() {
   return (
-    <>
-      <MainPage></MainPage>
-    </>
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <Routes>
+        <Route path={AppRoute.Root} element={<MainPage></MainPage>}></Route>
+        <Route path={AppRoute.Login} element={<LoginPage></LoginPage>}></Route>
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage></FavoritesPage>}
+        ></Route>
+        <Route path={AppRoute.Offer} element={<OfferPage></OfferPage>}></Route>
+        <Route path='*' element={<MainPage></MainPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
