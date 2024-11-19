5ce701fe8647314caa6a1a6d245e3a9f85bcab89
Kirill T <nan1911.work@gmail.com>
2024-11-19 17:16:40 +0500

    style bugfix

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -181,7 +181,7 @@ function MainPage(): JSX.Element {
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style={{width: `{80}%`}}></span>
+                        <span style={{width: `${80}%`}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
