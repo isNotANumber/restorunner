1129511386fcc67a1130b50d279c9ea7b869738b
Kirill T <nan1911.work@gmail.com>
2024-11-20 14:10:46 +0500

    header added

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -3,6 +3,24 @@ import { Link } from 'react-router-dom';
 function NotFoundPage(): JSX.Element {
   return (
     <div className='page page--gray page--not-found'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header__wrapper'>
+            <div className='header__left'>
+              <a className='header__logo-link' href='main.html'>
+                <img
+                  className='header__logo'
+                  src='img/icons/logo.svg'
+                  alt='Restorunner logo'
+                  width='171'
+                  height='41'
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+
       <div className='page__not-found-container container'>
         <main className='page__main'>
           <section className='not-found'>
@@ -11,7 +29,7 @@ function NotFoundPage(): JSX.Element {
               Sorry, the page you are looking for does not exist.
             </p>
             <Link className='not-found__link' to='/'>
-              Go to Home Page
+              To Home Page
             </Link>
           </section>
         </main>
