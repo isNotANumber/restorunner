ded18d6fd26ec4cc504363f4fd4d4668a631005d
Kirill T <nan1911.work@gmail.com>
2024-11-22 12:23:14 +0500

    unnecessary prop removed

--- src/components/place-cards-list/PlaceCardsList.tsx
+++ src/components/place-cards-list/PlaceCardsList.tsx
@@ -4,13 +4,11 @@ import PlaceCard from '../place-card/PlaceCard';
 type PlaceCardsListProps = {
   offers: Offers;
   page: 'rest-places' | 'favorites';
-  onMouseEnter?: (id: string) => void;
 };
 
 function PlaceCardsList({
   offers,
   page,
-  onMouseEnter,
 }: PlaceCardsListProps): JSX.Element {
   return (
     <div
@@ -22,7 +20,6 @@ function PlaceCardsList({
     >
       {offers.map((offer: Offer) => (
         <PlaceCard
-          onMouseEnter={onMouseEnter}
           key={offer.id}
           offer={offer}
           page={page}
