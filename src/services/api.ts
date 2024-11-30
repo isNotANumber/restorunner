4e75ad3de694c4b97c0fdd23596f7870e2ce7bb7
NaN <nan1911.work@gmail.com>
2024-11-30 14:00:05 +0500

    add and configure axios

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from "axios";
+
+const BACKEND_URL = "http://localhost:3000/";
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+	const api = axios.create({
+		baseURL: BACKEND_URL,
+		timeout: REQUEST_TIMEOUT,
+	});
+
+	return api;
+};
