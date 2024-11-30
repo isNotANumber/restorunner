99339cd159cc38b8e79fb2ebdcc312b1c5834318
NaN <nan1911.work@gmail.com>
2024-11-30 14:05:49 +0500

    redux thunk added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import catalogReducer from '../features/catalog/catalogSlice';
-import authReducer from '../features/auth/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import catalogReducer from "../features/catalog/catalogSlice";
+import authReducer from "../features/auth/authSlice";
+import { createAPI } from "../services/api";
+
+export const api = createAPI();
 
 export const store = configureStore({
-  reducer: {
-    catalog: catalogReducer,
-    auth: authReducer,
-  },
+	reducer: {
+		catalog: catalogReducer,
+		auth: authReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			thunk: {
+				extraArgument: api,
+			},
+		}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
