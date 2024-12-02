81d72db3d57b40fe994d1052999d125dd9bf7a35
NaN <nan1911.work@gmail.com>
2024-12-02 21:10:03 +0500

    refactoring

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,16 +1,9 @@
-const AUTH_TOKEN_KEY_NAME = "restorunner-token";
+const AUTH_TOKEN_KEY = "restorunner-token";
 
 export type Token = string;
 
-export const getToken = (): Token => {
-	const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
-	return token ?? "";
-};
+const getToken = (): Token => localStorage.getItem(AUTH_TOKEN_KEY) ?? "";
+const saveToken = (token: Token): void => localStorage.setItem(AUTH_TOKEN_KEY, token);
+const dropToken = (): void => localStorage.removeItem(AUTH_TOKEN_KEY);
 
-export const saveToken = (token: Token): void => {
-	localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
-};
-
-export const dropToken = (): void => {
-	localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
-};
+export { getToken, saveToken, dropToken };
