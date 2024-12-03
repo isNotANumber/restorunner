ceb212df6c296f434a021576f7a74151d7624ebb
NaN <nan1911.work@gmail.com>
2024-12-03 19:14:15 +0500

    auth bugfix

--- src/services/api.ts
+++ src/services/api.ts
@@ -30,7 +30,7 @@ export const createAPI = (): AxiosInstance => {
 	api.interceptors.request.use((config) => {
 		const token = getToken();
 		if (token && config.headers) {
-			config.headers["X-token"] = token;
+			config.headers["Authorization"] = `Bearer: ${token}`;
 		}
 
 		return config;
