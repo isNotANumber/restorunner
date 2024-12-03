6c4c19ac22a5da1f7460bf03926d19818e09a231
NaN <nan1911.work@gmail.com>
2024-12-03 18:44:45 +0500

    react toastify added

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./components/app/App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<ToastContainer />
 			<App></App>
 		</Provider>
 	</React.StrictMode>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,20 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import { StatusCodes } from "http-status-codes";
 import { getToken } from "./token";
+import { toast } from "react-toastify";
+
+type DetailMessageType = {
+	type: string;
+	message: string;
+};
+
+const StatusCodeMapping: Record<number, boolean> = {
+	[StatusCodes.BAD_REQUEST]: true,
+	[StatusCodes.UNAUTHORIZED]: true,
+	[StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 const enum Default {
 	BaseUrl = "http://localhost:3000",
@@ -20,6 +35,19 @@ export const createAPI = (): AxiosInstance => {
 
 		return config;
 	});
+
+	api.interceptors.response.use(
+		(response) => response,
+		(error: AxiosError<DetailMessageType>) => {
+			if (error.response && shouldDisplayError(error.response)) {
+				const detailMessage = error.response.data;
+
+				toast.warn(detailMessage.message);
+			}
+
+			throw error;
+		}
+	);
 
 	return api;
 };
