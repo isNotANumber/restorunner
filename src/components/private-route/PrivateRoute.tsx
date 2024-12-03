e2216e12e47b056401a591c45ba54df6321e2c30
NaN <nan1911.work@gmail.com>
2024-12-03 17:53:54 +0500

    fixes

--- src/components/private-route/PrivateRoute.tsx
+++ src/components/private-route/PrivateRoute.tsx
@@ -20,11 +20,6 @@ export default function PrivateRoute({ children, onlyUnAuth }: PrivateRouteProps
 	const location: Location<FromState> = useLocation() as Location<FromState>;
 
 	const user = useAppSelector(userSelectors.user);
-	// const userCheckAuth = useAppSelector(userSelectors.userStatus);
-
-	// if (userCheckAuth === AuthorizstionStatus.Unknown) {
-	// 	return <div>*Loader placeholder*</div>;
-	// }
 
 	if (onlyUnAuth && user) {
 		const from = location.state?.from || { pathname: AppRoute.Root };
